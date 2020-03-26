aa34a1237fcbc9b297fc8aa9697c891f1c20eb9b
3m1n3nc3 <mygames.ng@gmail.com>
2020-03-26 12:32:45 +0100

    Added number formating for download count, and also remove hidden style on stats bar on small screens

--- app/vendor/utils.php
+++ app/vendor/utils.php
@@ -259,6 +259,43 @@ function format_bytes($bytes, $force_unit = NULL, $format = NULL, $si = TRUE)
 
     return sprintf($format, $bytes / pow($mod, $power), $units[$power]);
 }
+
+// Hugh number formatter  
+function numberFormater($n, $full = 0, $precision = 1) {
+    if ($full == 1) {
+        $n_format = number_format($n);
+        $suffix = '';
+    } elseif ($n < 900) {
+        // 0 - 900
+        $n_format = number_format($n, $precision);
+        $suffix = '';
+    } else if ($n < 900000) {
+        // 0.9k-850k
+        $n_format = number_format($n / 1000, $precision);
+        $suffix = 'K';
+    } else if ($n < 900000000) {
+        // 0.9m-850m
+        $n_format = number_format($n / 1000000, $precision);
+        $suffix = 'M';
+    } else if ($n < 900000000000) {
+        // 0.9b-850b
+        $n_format = number_format($n / 1000000000, $precision);
+        $suffix = 'B';
+    } else {
+        // 0.9t+
+        $n_format = number_format($n / 1000000000000, $precision);
+        $suffix = 'T';
+    }
+      // Remove unnecessary zeros after decimal. "1.0" -> "1"; "1.00" -> "1"
+      // Intentionally does not affect partials, eg "1.50" -> "1.50"
+    if ( $precision > 0 ) {
+        $dotzero = '.' . str_repeat( '0', $precision );
+        $n_format = str_replace( $dotzero, '', $n_format );
+    }
+    
+    return $n_format . $suffix;
+}
+    
 function autoLinkUrls($text, $popup = true)
 {
     $target = false;
